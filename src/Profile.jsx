3d636210b532603c73d050c71d103ad6a666876d
Dhaka-yahyaoui <dhakayahyaoui54@gmail.com>
2024-01-30 21:05:29 +0100

    statechekpoint

---
+++ src/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfileImg = () => {
+  return (
+    <div>
+      <img src="https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg" alt="" />
+    </div>
+  )
+}
+
+export default ProfileImg
